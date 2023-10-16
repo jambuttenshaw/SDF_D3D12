2e836d9163da5d5558b7f8ab0e2e367653254280
jambuttenshaw <jamie.buttenshaw@gmail.com>
2023-10-16 14:19:56 +0100

    Implemented frame buffering and first triangle

--- sdf_d3d12/src/Application/D3D12Application.cpp
+++ sdf_d3d12/src/Application/D3D12Application.cpp
@@ -6,10 +6,10 @@
 
 D3D12Application::D3D12Application(UINT width, UINT height, const std::wstring& name)
 	: BaseApplication(width, height, name)
+	, m_Viewport(0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height))
+	, m_ScissorRect(0, 0, static_cast<LONG>(width), static_cast<LONG>(height))
 	, m_FenceEvent(nullptr)
-	, m_FenceValue(0)
 {
-	
 }
 
 void D3D12Application::OnInit()
@@ -116,29 +116,117 @@ void D3D12Application::LoadPipeline()
 			THROW_IF_FAIL(m_SwapChain->GetBuffer(n, IID_PPV_ARGS(&m_RenderTargets[n])));
 			m_Device->CreateRenderTargetView(m_RenderTargets[n].Get(), nullptr, rtvHandle);
 			rtvHandle.Offset(1, m_RTVDescriptorSize);
+			
+			// Create command allocator
+			THROW_IF_FAIL(m_Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_CommandAllocators[n])));
 		}
 	}
 
-	// Create command allocator
-	THROW_IF_FAIL(m_Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_CommandAllocator)));
 }
 
 void D3D12Application::LoadAssets()
 {
+	// Create an empty root signature
+	{
+		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+		rootSignatureDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+		ComPtr<ID3DBlob> signature;
+		ComPtr<ID3DBlob> error;
+		THROW_IF_FAIL(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+		THROW_IF_FAIL(m_Device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_RootSignature)));
+	}
+
+	// Create the pipeline state, which includes compiling and loading shaders
+	{
+		ComPtr<ID3DBlob> vertexShader;
+		ComPtr<ID3DBlob> pixelShader;
+
+#ifdef _DEBUG
+		UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#else
+		UINT compileFlags = 0;
+#endif
+
+		// Compile shaders
+		THROW_IF_FAIL(D3DCompileFromFile(L"assets/shaders/shaders.hlsl", nullptr, nullptr, "VSMain", "vs_5_0", compileFlags, 0, &vertexShader, nullptr));
+		THROW_IF_FAIL(D3DCompileFromFile(L"assets/shaders/shaders.hlsl", nullptr, nullptr, "PSMain", "ps_5_0", compileFlags, 0, &pixelShader, nullptr));
+
+		// Define the vertex input layout
+		D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
+		{
+			{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+			{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+		};
+
+		// Describe and create the graphics pipeline state object (PSO)
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+		psoDesc.InputLayout = { inputElementDescs, _countof(inputElementDescs) };
+		psoDesc.pRootSignature = m_RootSignature.Get();
+		psoDesc.VS = CD3DX12_SHADER_BYTECODE(vertexShader.Get());
+		psoDesc.PS = CD3DX12_SHADER_BYTECODE(pixelShader.Get());
+		psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+		psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+		psoDesc.DepthStencilState.DepthEnable = FALSE;
+		psoDesc.DepthStencilState.StencilEnable = FALSE;
+		psoDesc.SampleMask = UINT_MAX;
+		psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+		psoDesc.NumRenderTargets = 1;
+		psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+		psoDesc.SampleDesc.Count = 1;
+		THROW_IF_FAIL(m_Device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&m_PipelineState)));
+	}
+
 	// Create the command list
 	THROW_IF_FAIL(m_Device->CreateCommandList(0,
 		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		m_CommandAllocator.Get(), nullptr,
+		m_CommandAllocators[m_FrameIndex].Get(), nullptr,
 		IID_PPV_ARGS(&m_CommandList)));
 
 	// Close the command list, as the main loop expects it to be closed
 	THROW_IF_FAIL(m_CommandList->Close());
 
+	// Create the vertex buffer
+	{
+		// Define the geometry for a triangle.
+		Vertex triangleVertices[] =
+		{
+			{ { 0.0f, 0.25f * m_AspectRatio, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+			{ { 0.25f, -0.25f * m_AspectRatio, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+			{ { -0.25f, -0.25f * m_AspectRatio, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+		};
+
+		constexpr UINT vertexBufferSize = sizeof(triangleVertices);
+
+		CD3DX12_HEAP_PROPERTIES heapProperties(D3D12_HEAP_TYPE_UPLOAD);
+		auto resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize);
+
+		THROW_IF_FAIL(m_Device->CreateCommittedResource(
+			&heapProperties,
+			D3D12_HEAP_FLAG_NONE,
+			&resourceDesc,
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&m_VertexBuffer)));
+
+		// Copy the triangle data to the vertex buffer
+		UINT8* pVertexDataBegin;
+		CD3DX12_RANGE readRange(0, 0); // We do not intend to read from this resource on the CPU
+		THROW_IF_FAIL(m_VertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
+		memcpy(pVertexDataBegin, triangleVertices, sizeof(triangleVertices));
+		m_VertexBuffer->Unmap(0, nullptr);
+
+		// Initialize the vertex buffer view
+		m_VertexBufferView.BufferLocation = m_VertexBuffer->GetGPUVirtualAddress();
+		m_VertexBufferView.StrideInBytes = sizeof(Vertex);
+		m_VertexBufferView.SizeInBytes = vertexBufferSize;
+	}
+
 	// Create synchronization objects
 	{
 		THROW_IF_FAIL(m_Device->CreateFence(0,
 			D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_Fence)));
-		m_FenceValue = 1;
+		m_FenceValues[m_FrameIndex] = 1;
 
 		// Create an event handle to use for frame synchronization
 		m_FenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
@@ -146,6 +234,9 @@ void D3D12Application::LoadAssets()
 		{
 			THROW_IF_FAIL(HRESULT_FROM_WIN32(GetLastError()));
 		}
+
+		// Wait for the command list to execute
+		WaitForGPU();
 	}
 }
 
@@ -167,14 +258,14 @@ void D3D12Application::OnRender()
 	// Present the frame
 	THROW_IF_FAIL(m_SwapChain->Present(1, 0));
 
-	WaitForPreviousFrame();
+	MoveToNextFrame();
 }
 
 void D3D12Application::OnDestroy()
 {
 	// Ensure the GPU is no longer references resources that are about
 	// to be cleaned up by the destructor
-	WaitForPreviousFrame();
+	WaitForGPU();
 
 	CloseHandle(m_FenceEvent);
 }
@@ -183,40 +274,65 @@ void D3D12Application::PopulateCommandList()
 {
 	// Command list allocators can only be reset when the associated
 	// command lists have finished execution on the GPU
-	THROW_IF_FAIL(m_CommandAllocator->Reset());
+	THROW_IF_FAIL(m_CommandAllocators[m_FrameIndex]->Reset());
 
 	// Command lists can (and must) be reset after ExecuteCommandList() is called and before it is repopulated
-	THROW_IF_FAIL(m_CommandList->Reset(m_CommandAllocator.Get(), m_PipelineState.Get()));
+	THROW_IF_FAIL(m_CommandList->Reset(m_CommandAllocators[m_FrameIndex].Get(), m_PipelineState.Get()));
+
+	// Set necessary state
+	m_CommandList->SetGraphicsRootSignature(m_RootSignature.Get());
+	m_CommandList->RSSetViewports(1, &m_Viewport);
+	m_CommandList->RSSetScissorRects(1, &m_ScissorRect);
 
 	// Indicate the back buffer will be used as a render target
 	auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_RenderTargets[m_FrameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
 	m_CommandList->ResourceBarrier(1, &barrier);
 
 	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_RTVHeap->GetCPUDescriptorHandleForHeapStart(), m_FrameIndex, m_RTVDescriptorSize);
+	m_CommandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
 
 	// record commands
 	constexpr float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
 	m_CommandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
 
+	// render the triangle
+	m_CommandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	m_CommandList->IASetVertexBuffers(0, 1, &m_VertexBufferView);
+	m_CommandList->DrawInstanced(3, 1, 0, 0);
+
 	// Indicate that the back buffer will now be used to present
-	barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_RenderTargets[m_FrameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_RenderTargets[m_FrameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
 	m_CommandList->ResourceBarrier(1, &barrier);
 
 	THROW_IF_FAIL(m_CommandList->Close());
 }
 
-void D3D12Application::WaitForPreviousFrame()
+void D3D12Application::WaitForGPU()
 {
 	// Signal and increment the fence
-	const UINT64 fence = m_FenceValue;
-	THROW_IF_FAIL(m_CommandQueue->Signal(m_Fence.Get(), fence));
-	m_FenceValue++;
+	THROW_IF_FAIL(m_CommandQueue->Signal(m_Fence.Get(), m_FenceValues[m_FrameIndex]));
 
-	if (m_Fence->GetCompletedValue() < fence)
+	THROW_IF_FAIL(m_Fence->SetEventOnCompletion(m_FenceValues[m_FrameIndex], m_FenceEvent));
+	WaitForSingleObject(m_FenceEvent, INFINITE);
+
+	m_FenceValues[m_FrameIndex]++;
+}
+
+void D3D12Application::MoveToNextFrame()
+{
+	const UINT64 currentFenceValue = m_FenceValues[m_FrameIndex];
+	THROW_IF_FAIL(m_CommandQueue->Signal(m_Fence.Get(), currentFenceValue));
+
+	// Update the frame index
+	m_FrameIndex = m_SwapChain->GetCurrentBackBufferIndex();
+
+	// if the next frame is not ready to be rendered yet, wait until it is ready
+	if (m_Fence->GetCompletedValue() < m_FenceValues[m_FrameIndex])
 	{
-		THROW_IF_FAIL(m_Fence->SetEventOnCompletion(fence, m_FenceEvent));
-		WaitForSingleObject(m_FenceEvent, INFINITE);
+		THROW_IF_FAIL(m_Fence->SetEventOnCompletion(m_FenceValues[m_FrameIndex], m_FenceEvent));
+		WaitForSingleObjectEx(m_FenceEvent, INFINITE, FALSE);
 	}
 
-	m_FrameIndex = m_SwapChain->GetCurrentBackBufferIndex();
+	// Set the fence value for the next frame
+	m_FenceValues[m_FrameIndex] = currentFenceValue + 1;
 }
--- sdf_d3d12/src/Application/D3D12Application.h
+++ sdf_d3d12/src/Application/D3D12Application.h
@@ -4,6 +4,7 @@
 
 
 using Microsoft::WRL::ComPtr;
+using namespace DirectX;
 
 
 class D3D12Application : public BaseApplication
@@ -22,28 +23,44 @@ private:
 	void LoadAssets();
 
 	void PopulateCommandList();
-	void WaitForPreviousFrame();
+
+	void WaitForGPU();
+	void MoveToNextFrame();
 
 private:
 	static constexpr UINT s_FrameCount = 2;
+
+	struct Vertex
+	{
+		XMFLOAT3 position;
+		XMFLOAT4 color;
+	};
 
 	// Pipeline objects
 	ComPtr<IDXGISwapChain3> m_SwapChain;
 	ComPtr<ID3D12Device> m_Device;
 	ComPtr<ID3D12Resource> m_RenderTargets[s_FrameCount];
 
-	ComPtr<ID3D12CommandAllocator> m_CommandAllocator;
+	ComPtr<ID3D12CommandAllocator> m_CommandAllocators[s_FrameCount];
 	ComPtr<ID3D12CommandQueue> m_CommandQueue;
 
+	ComPtr<ID3D12RootSignature> m_RootSignature;
 	ComPtr<ID3D12DescriptorHeap> m_RTVHeap;
 	UINT m_RTVDescriptorSize = 0;
 
 	ComPtr<ID3D12PipelineState> m_PipelineState;
 	ComPtr<ID3D12GraphicsCommandList> m_CommandList;
 
+	CD3DX12_VIEWPORT m_Viewport;
+	CD3DX12_RECT m_ScissorRect;
+
+	// App resources
+	ComPtr<ID3D12Resource> m_VertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW m_VertexBufferView;
+
 	// Synchronization objects
 	UINT m_FrameIndex = 0;
 	HANDLE m_FenceEvent;
 	ComPtr<ID3D12Fence> m_Fence;
-	UINT64 m_FenceValue;
+	UINT64 m_FenceValues[s_FrameCount] = { 0, 0 };
 };
