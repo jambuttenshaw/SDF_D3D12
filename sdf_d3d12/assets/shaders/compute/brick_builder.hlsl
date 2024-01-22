6680fd84a74645d2c3f22b0e1421109e480bd895
jambuttenshaw <jamie.buttenshaw@gmail.com>
2024-01-22 16:38:44 +0000

    Making brick builder independent of volume texture

--- sdf_d3d12/assets/shaders/compute/brick_builder.hlsl
+++ sdf_d3d12/assets/shaders/compute/brick_builder.hlsl
@@ -46,24 +46,11 @@ float EvaluateEditList(float3 p)
 }
 
 
-float FormatDistance(float inDistance, float volumeDimensions)
+float FormatDistance(float inDistance, float UVWExtent)
 {
-	// Texture format is 8-bit unsigned normalized
-	// Values range from 0 to 1, with 256 unique values being able to be stored in the texture
-
-	// Need to decide how to map distances such that the largest steps can be taken when sphere tracing
-	// But distances must still be conservative, otherwise the sphere-tracing will intersect with geometry
-
-	// In Claybook, a distance value of 1 means 4 voxels
-	// They used a signed normalized format, so they had [-4,+4] range with 256 values to represent it
-	// giving 1/32 voxel precision
-
-	// The space the SDFs were evaluated ranged from [-1,1]
-	// Therefore a value of 2 = volume resolution
-
 	// Calculate the distance value in terms of voxels
-	// This will be in the range [0, volume resolution]
-	const float voxelDistance = max(0.0f, min(volumeDimensions, inDistance * volumeDimensions / 2));
+	const float voxelsPerUnit = 0.5f * SDF_BRICK_SIZE / UVWExtent;
+	const float voxelDistance = max(0.0f, inDistance * voxelsPerUnit);
 
 	// Now map the distance such that 1 = maxDistance
 	return saturate(voxelDistance / SDF_VOLUME_STRIDE);
@@ -74,12 +61,9 @@ float FormatDistance(float inDistance, float volumeDimensions)
 void main(uint3 DTid : SV_DispatchThreadID, uint3 GroupID : SV_GroupID, uint3 GTid : SV_GroupThreadID)
 {
 	// TODO: Eventually this will be replaced as thread ID will no longer map to volume coordinates once bricks are stored in a brick pool
-	// Make sure thread has a pixel in the texture to process
 	uint3 dims;
 	g_OutputTexture.GetDimensions(dims.x, dims.y, dims.z);
-	//if (DTid.x >= dims.x || DTid.y >= dims.y || DTid.z >= dims.z)
-	//	return;
-
+	
 	// Get the bounding box that this group will fill
 	const AABBPrimitiveData primitiveData = g_PrimitiveData[GroupID.x];
 
@@ -95,7 +79,7 @@ void main(uint3 DTid : SV_DispatchThreadID, uint3 GroupID : SV_GroupID, uint3 GT
 
 	// Evaluate SDF volume
 	const float nearest = EvaluateEditList(uvw);
-	const float mappedDistance = FormatDistance(nearest, dims.x);
+	const float mappedDistance = FormatDistance(nearest, primitiveData.UVWExtent);
 
 	// Store the mapped distance in the volume
 	g_OutputTexture[voxel] = mappedDistance;
