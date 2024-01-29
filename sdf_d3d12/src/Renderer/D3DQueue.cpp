55ef943107f38122cf05152092cd1acb79db16d0
jambuttenshaw <jamie.buttenshaw@gmail.com>
2024-01-29 11:09:07 +0000

    Added queue/fence wrapper

---
+++ sdf_d3d12/src/Renderer/D3DQueue.cpp
@@ -0,0 +1,96 @@
+#include "pch.h"
+#include "D3DQueue.h"
+
+
+D3DQueue::D3DQueue(ID3D12Device* device, D3D12_COMMAND_LIST_TYPE type)
+	: m_QueueType(type)
+{
+	// Create queue
+	{
+		D3D12_COMMAND_QUEUE_DESC desc;
+		desc.Type = m_QueueType;
+		desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+		desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		desc.NodeMask = 0;
+		THROW_IF_FAIL(device->CreateCommandQueue(&desc, IID_PPV_ARGS(&m_CommandQueue)));
+	}
+
+	// Assign initial fence values
+	m_LastCompletedFenceValue = 0;
+	m_NextFenceValue = 1;
+
+	// Create fence
+	{
+		THROW_IF_FAIL(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_Fence)));
+		THROW_IF_FAIL(m_Fence->Signal(m_LastCompletedFenceValue));
+	}
+
+	// Create fence event
+	{
+		m_FenceEventHandle = CreateEventEx(nullptr, L"Queue Fence Event", 0, EVENT_ALL_ACCESS);
+		ASSERT(m_FenceEventHandle != INVALID_HANDLE_VALUE, "Failed to create fence event.");
+	}
+}
+
+D3DQueue::~D3DQueue()
+{
+	CloseHandle(m_FenceEventHandle);
+}
+
+UINT64 D3DQueue::ExecuteCommandLists(UINT count, ID3D12CommandList** ppCommandLists)
+{
+	m_CommandQueue->ExecuteCommandLists(count, ppCommandLists);
+
+	std::lock_guard lock(m_FenceMutex);
+	m_CommandQueue->Signal(m_Fence.Get(), m_NextFenceValue);
+
+	return m_NextFenceValue++;
+}
+
+void D3DQueue::InsertWait(UINT64 fenceValue) const
+{
+	THROW_IF_FAIL(m_CommandQueue->Wait(m_Fence.Get(), fenceValue));
+}
+
+void D3DQueue::InsertWaitForQueueFence(const D3DQueue* otherQueue, UINT64 fenceValue) const
+{
+	THROW_IF_FAIL(m_CommandQueue->Wait(otherQueue->GetFence(), fenceValue));
+
+}
+
+void D3DQueue::InsertWaitForQueue(const D3DQueue* otherQueue) const
+{
+	THROW_IF_FAIL(m_CommandQueue->Wait(otherQueue->GetFence(), otherQueue->GetNextFenceValue() - 1));
+}
+
+void D3DQueue::WaitForFenceCPUBlocking(UINT64 fenceValue)
+{
+	if (IsFenceComplete(fenceValue))
+	{
+		return;
+	}
+
+	{
+		std::lock_guard lockGuard(m_EventMutex);
+
+		m_Fence->SetEventOnCompletion(fenceValue, m_FenceEventHandle);
+		WaitForSingleObjectEx(m_FenceEventHandle, INFINITE, false);
+		m_LastCompletedFenceValue = fenceValue;
+	}
+}
+
+bool D3DQueue::IsFenceComplete(UINT64 fenceValue)
+{
+	if (fenceValue > m_LastCompletedFenceValue)
+	{
+		PollCurrentFenceValue();
+	}
+
+	return fenceValue <= m_LastCompletedFenceValue;
+}
+
+UINT64 D3DQueue::PollCurrentFenceValue()
+{
+	m_LastCompletedFenceValue = max(m_LastCompletedFenceValue, m_Fence->GetCompletedValue());
+	return m_LastCompletedFenceValue;
+}
---
+++ sdf_d3d12/src/Renderer/D3DQueue.h
@@ -0,0 +1,53 @@
+#pragma once
+
+using Microsoft::WRL::ComPtr;
+
+
+class D3DQueue
+{
+public:
+	D3DQueue(ID3D12Device* device, D3D12_COMMAND_LIST_TYPE type);
+	~D3DQueue();
+
+	DISALLOW_COPY(D3DQueue)
+	DEFAULT_MOVE(D3DQueue)
+	
+	// Execute work
+	UINT64 ExecuteCommandLists(UINT count, ID3D12CommandList** ppCommandLists);
+
+	// GPU wait
+	void InsertWait(UINT64 fenceValue) const;
+	void InsertWaitForQueueFence(const D3DQueue* otherQueue, UINT64 fenceValue) const;
+	void InsertWaitForQueue(const D3DQueue* otherQueue) const;
+
+	// CPU wait
+	void WaitForFenceCPUBlocking(UINT64 fenceValue);
+	inline void WaitForIdleCPUBlocking() { WaitForFenceCPUBlocking(m_NextFenceValue - 1); }
+
+	// Polling
+	bool IsFenceComplete(UINT64 fenceValue);
+	UINT64 PollCurrentFenceValue();
+	inline UINT64 GetLastCompletedFence() const { return m_LastCompletedFenceValue; }
+	inline UINT64 GetNextFenceValue() const { return m_NextFenceValue; }
+
+	// Resources
+	inline ID3D12CommandQueue* GetCommandQueue() const { return m_CommandQueue.Get(); }
+	inline ID3D12Fence* GetFence() const { return m_Fence.Get(); }
+
+
+private:
+	// D3D12 resources
+	ComPtr<ID3D12CommandQueue> m_CommandQueue;
+	ComPtr<ID3D12Fence> m_Fence;
+
+	D3D12_COMMAND_LIST_TYPE m_QueueType;
+
+	// Mutexes
+	std::mutex m_EventMutex;
+	std::mutex m_FenceMutex;
+
+	UINT64 m_NextFenceValue = -1;
+	UINT64 m_LastCompletedFenceValue = -1;
+
+	HANDLE m_FenceEventHandle = nullptr;
+};
