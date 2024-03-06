1ef3749c8aa8a4193358cf72bc47960871ff1ae6
jambuttenshaw <jamie.buttenshaw@gmail.com>
2024-03-06 13:43:00 +0000

    Restored profile config

---
+++ sdf_d3d12/profile_config/profile_compute.bat
@@ -0,0 +1,3 @@
+cd /d %~dp0
+cd ../
+START /B ./../x64/Profile/sdf_d3d12.exe --profile-config="profile_config/profile.json" --gpu-profiler-config="profile_config/compute_gpuprofile.json" --profile-output="captures/profile_compute.csv"
---
+++ sdf_d3d12/profile_config/profile_direct.bat
@@ -0,0 +1,3 @@
+cd /d %~dp0
+cd ../
+START /B ./../x64/Profile/sdf_d3d12.exe --profile-config="profile_config/profile.json" --gpu-profiler-config="profile_config/direct_gpuprofile.json" --profile-output="captures/profile_direct.csv"
